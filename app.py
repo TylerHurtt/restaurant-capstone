a6e88a4a14793fe05ade6d851181a9f33e0e946f
Tyler Hurtt <TylerAdamHurtt@gmail.com>
2020-10-21 19:37:10 -0400

    bring back track modifications

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
 moment = Moment(app)
 # app.config.from_object('config')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.debug = False
 
 # app = Flask(__name__)
