4fe6a819f32f821446f1b7a313165f57dc664093
Tyler Hurtt <TylerAdamHurtt@gmail.com>
2020-10-21 17:43:16 -0400

    added config.py and recreated app.py

--- app.py
+++ app.py
@@ -1,16 +1,85 @@
-import os
-from flask import Flask, request, abort, jsonify
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+import json
+import dateutil.parser
+import babel
+import sys
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify, abort
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+import logging
+from logging import Formatter, FileHandler
+from flask_wtf import Form
+from flask_migrate import Migrate
+from datetime import date, datetime
+from sqlalchemy import join
+from sqlalchemy.orm import sessionmaker
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
 
-  return app
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
 
-APP = create_app()
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+# app = Flask(__name__)
+
+# app.config.from_object('config')
+# app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://Tyler:Pesthlos!2772@localhost:5432/fyyur'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.debug = True
+
+db = SQLAlchemy(app)
+
+# TODO: connect to a local postgresql database
+
+migrate = Migrate(app, db)
+
+
+@app.route('/')
+def index():
+    print('hello world')
+    return 'hello world'
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('errors/500.html'), 500
+
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+# Default port:
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
